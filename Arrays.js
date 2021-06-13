5885e3a06f6f4353700cd38fd3daf559bda13ffe
Elisangela34 <85572018+Elisangela34@users.noreply.github.com>
2021-06-12 21:19:50 -0300

    adicionando exercício sobre array do playground

---
+++ Arrays.js
@@ -0,0 +1,20 @@
+
+
+
+let produtos = ['amaciante', 'sabao', 'detergente', 'shampoo', 'sabonete'];
+console.log(produtos)
+console.log(produtos.pop(), "O método 'pop' remove e retorna o último item de uma matriz.")
+console.log(produtos)
+console.log(produtos.join(" — "), "O método 'join' converte uma matriz em uma string, unindo todos os itens com um sinal que pode ser definido como argumento.")
+console.log(produtos)
+console.log(produtos.push("condicionador"), "O método 'push' acrescenta um item ao final da matriz e retorna quantos itens existem nela.")
+console.log(produtos)
+console.log(produtos.shift(), "O método 'shift' remove e retorna o primeiro item da matriz.")
+console.log(produtos)
+console.log(produtos.unshift("pasta de dente"), "'unshift' acrescente um item no início da matriz e retorna quantos items existem nela.")
+console.log(produtos)
+
+
+
+
+
